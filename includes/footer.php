80c8a5e15dbb8bee6967bc68fea0b5a72fdc8899
PolinaTimofeeva <polleyw@mail.ru>
2022-04-06 15:06:02 +0300

    тестовый хедер

--- includes/footer.php
+++ includes/footer.php
@@ -2,11 +2,23 @@
     <div class="container">
         <div class="footer__body">
             <div class="footer__left">
-                <h5 class="footer__title">Получите консультацию по вашему бизнесу прямо сейчас</h5>
-                <img src="../src/telegram.svg" alt="Telegram" />
+                <div class="footer__left-up">
+                    <h5 class="footer__title">Получите консультацию по вашему бизнесу прямо сейчас</h5>
+                    <a href="" class="telegram"><img class="telegram" src="../src/telegram.svg" alt="Telegram" /></a>
+                </div>
+                <div class="footer__left-down">
+                    <a href="tel:84951183934" class="footer__phone">8 495 118 39 34</a>
+                </div>
             </div>
             <div class="footer__right">
-
+                <img class="logo" src="../src/logo.svg" alt="Логотип MYFORCE">
+                <h5 class="footer__sub">Экосистема для вашего бизнеса</h5>
+                <p class="footer__remark">*отправляя формы на данном сайте, вы даете согласие на обработку персональных данных в соответствии с 152-ФЗ</p>
+                <div class="footer__right-down">
+                    <a href="" class="footer__right-item">Политика конфиденциальности</a>
+                    <a href="" class="footer__right-item">ИНН: 6167130086</a>
+                    <a href="" class="footer__right-item">ОГРН: 1156196049415</a>
+                </div>
             </div>
         </div>
     </div>
---
+++ includes/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Веб-переезд</title>
+
+    <meta name="viewport" content="width=device-width">
+
+    <link rel="stylesheet" href="../styles/reset.css">
+    <link rel="stylesheet" href="../styles/quiz.css">
+    <link rel="stylesheet" href="../styles/main.css">
+</head>
+<body>
+<header class="wrapper">
+    <div class="header__outer d-flex justify-content-between align-items-center header__spacing mt-4 w-75">
+        <div class="header__inner d-flex align-items-center">
+            <div class="header__item mr-5">
+                <a href="/" class="logo">MYFORCE</a>
+                <p class="logo__text">Экосистема для вашего бизнеса</p>
+            </div>
+
+            <a href="https://t.me/myforce_business" target="_blank" class="header__item logo-tg align-self-center ml-5">
+                <svg width="21" height="18" viewBox="0 0 21 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M20.7332 1.76099C20.8891 0.754147 19.9318 -0.040558 19.0365 0.352494L1.20602 8.18105C0.564035 8.46292 0.610995 9.43532 1.27683 9.64736L4.95393 10.8183C5.65573 11.0418 6.41566 10.9263 7.02848 10.5029L15.3187 4.77534C15.5687 4.60262 15.8412 4.95808 15.6276 5.17829L9.66014 11.3308C9.08126 11.9276 9.19616 12.9389 9.89246 13.3756L16.5737 17.5654C17.3231 18.0353 18.2871 17.5632 18.4273 16.6577L20.7332 1.76099Z" fill="#30B3F5"/>
+                </svg>
+            </a>
+        </div>
+
+        <div class="header__inner">
+            <div class="header__item phone">
+                <a href="tel:8 495 118 39 34" class="phone">8 495 118 39 34</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="onHeader">
+        <p>123</p>
+    </div>
+
+    <div class="header__line"></div>
+</header>

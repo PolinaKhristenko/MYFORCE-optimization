ae51701e49f10dfc3d7f9367e1982ec314c3aa68
PolinaTimofeeva <polleyw@mail.ru>
2022-04-06 21:26:09 +0300

    телега svg

--- includes/header.php
+++ includes/header.php
@@ -24,9 +24,10 @@
                 <div class="header__inner">
                     <div class="header__item links">
                         <a href="https://t.me/masalskymir" target="_blank" class="header__item telegram">
-                            <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <circle cx="13" cy="13" r="11.75" fill="white" stroke="white"/>
-                            </svg>
+                        <svg class="telegram" width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="14" cy="14" r="11.75" stroke="white"/>
+                            <path d="M20.1132 8.93269C20.2223 8.2279 19.5522 7.67161 18.9256 7.94675L6.44422 13.4267C5.99482 13.624 6.0277 14.3047 6.49378 14.4532L9.06775 15.2728C9.55901 15.4293 10.091 15.3484 10.5199 15.052L16.3231 11.0427C16.4981 10.9218 16.6888 11.1707 16.5393 11.3248L12.3621 15.6316C11.9569 16.0493 12.0373 16.7573 12.5247 17.0629L17.2016 19.9958C17.7261 20.3247 18.401 19.9942 18.4991 19.3604L20.1132 8.93269Z" fill="white"/>
+                        </svg>
                         </a>
                         <a href="tel:8 495 118 39 34" class="phone">8 495 118 39 34</a>
                     </div>

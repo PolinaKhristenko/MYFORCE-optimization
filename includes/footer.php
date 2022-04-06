afe44497a69f1aae8aa5693c9f90281f21cfacd6
PolinaTimofeeva <polleyw@mail.ru>
2022-04-06 14:38:13 +0300

    подключила шрифт и инклюдесы

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,8 @@
     <div class="container">
         <div class="footer__body">
             <div class="footer__left">
-                
+                <h5 class="footer__title">Получите консультацию по вашему бизнесу прямо сейчас</h5>
+                <img src="../src/telegram.svg" alt="Telegram" />
             </div>
             <div class="footer__right">
 
--- index.php
+++ index.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@300;400;600;700&display=swap" rel="stylesheet">
+
     <link href='./styles/reset.css' type='text/css' rel='stylesheet' />
     <link href='./styles/main.css' type='text/css' rel='stylesheet' />
     <link href='./styles/quiz.css' type='text/css' rel='stylesheet' />
@@ -11,9 +15,14 @@
 </head>
 <body>
     <div class="wrapper">
+        <?php include'./includes/header.php'?> <!-- Хэдер -->
 
+        <main class="main">
+            
+        </main>
+
+        <?php include'./includes/footer.php'?> <!-- Футер -->
     </div>
-
     <script src="./js/main.js"></script>
     <script src="./js/quiz.js"></script>
 </body>

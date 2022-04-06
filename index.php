dc72834927205577c4ff09391a7f3f9677e82cbd
PolinaTimofeeva <polleyw@mail.ru>
2022-04-06 14:14:22 +0300

    подключила файлы

--- index.php
+++ index.php
@@ -4,9 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='./styles/reset.css' type='text/css' rel='stylesheet' />
+    <link href='./styles/main.css' type='text/css' rel='stylesheet' />
+    <link href='./styles/quiz.css' type='text/css' rel='stylesheet' />
     <title>MYFORCE</title>
 </head>
 <body>
-    
+    <div class="wrapper">
+
+    </div>
+
+    <script src="./js/main.js"></script>
+    <script src="./js/quiz.js"></script>
 </body>
 </html>

31aad9e9b5f12ea9893a2cd752a6b7a13d969151
PolinaTimofeeva <polleyw@mail.ru>
2022-04-06 14:11:30 +0300

    зашеренные иконки и лого

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+<footer class="footer">
+    <div class="container">
+        <div class="footer__body">
+            <div class="footer__left">
+                
+            </div>
+            <div class="footer__right">
+
+            </div>
+        </div>
+    </div>
+</footer>

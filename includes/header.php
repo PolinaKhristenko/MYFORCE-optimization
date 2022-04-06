5aebdb265442c6d2d9df6c7b55ff2ad995c52fcf
Silver <gramakov27@gmail.com>
2022-04-06 18:04:55 +0300

    доделан хедер со стилями

--- includes/header.php
+++ includes/header.php
@@ -18,14 +18,12 @@
             <div class="header__inner d-flex align-items-center">
                 <div class="header__item">
                     <img class="logo" src="../src/logo.svg" alt="Логотип MYFORCE">
-                    <p class="logo__text">Экосистема для вашего бизнеса</p>
+                    <p class="header__sub">Экосистема для вашего бизнеса</p>
                 </div>
-
-
             </div>
 
             <div class="header__inner">
-                <div class="header__item">
+                <div class="header__item links">
                     <a href="https://t.me/masalskymir" target="_blank" class="header__item telegram">
                         <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M14.1132 1.93269C14.2223 1.2279 13.5522 0.671609 12.9256 0.946746L0.444216 6.42674C-0.00517519 6.62404 0.0276966 7.30473 0.493784 7.45315L3.06775 8.27284C3.55901 8.42928 4.09096 8.34839 4.51994 8.05202L10.3231 4.04274C10.4981 3.92184 10.6888 4.17065 10.5393 4.3248L6.3621 8.63157C5.95688 9.04935 6.03731 9.75726 6.52472 10.0629L11.2016 12.9958C11.7261 13.3247 12.401 12.9942 12.4991 12.3604L14.1132 1.93269Z" fill="white"/>
@@ -36,9 +34,7 @@
             </div>
         </div>
 
-        <div class="header__bookmark">
-            <p>123</p>
-        </div>
+        <div class="header__bookmark"></div>
 
         <div class="header__line"></div>
     </div>

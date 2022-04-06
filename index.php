0fb2397a2b4c56b5de8e41732575ffa68dc99c25
PolinaTimofeeva <polleyw@mail.ru>
2022-04-06 21:19:20 +0300

    синхрон для пулла

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php include'./includes/header.php'?> <!-- Хэдер -->
     <div class="wrapper">
         <main class="main">
+            <section class="">
+
+            </section>
             
         </main>
         <?php include'./includes/footer.php'?> <!-- Футер -->
